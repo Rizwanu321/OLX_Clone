6bc94e343e20304e34ac41c9662530f2ce54b437
Rizwanu321 <Rizwanurahman321@gmail.com>
2023-08-15 19:50:45 +0530

    final code
    OLX_Clone

---
+++ src/Components/View/View.js
@@ -0,0 +1,43 @@
+import React,{useContext,useState,useEffect} from 'react';
+import { PostContext } from '../../store/postContext';
+import './View.css';
+import { FirebaseContext } from '../../store/Context';
+function View() {
+  const [userDetails,setUserDetails] = useState()
+  const {postDetails} = useContext(PostContext)
+  const {firebase} = useContext(FirebaseContext)
+  useEffect(()=>{
+    const {userId} = postDetails
+    firebase.firestore().collection('users').where('id','==',postDetails.userId).get().then((res)=>{
+      res.forEach(doc => {
+        setUserDetails(doc.data())
+      })
+    })
+
+  },[])
+
+  return (
+    <div className="viewParentDiv">
+      <div className="imageShowDiv">
+        <img
+          src={postDetails.url}
+          alt=""
+        />
+      </div>
+      <div className="rightSection">
+        <div className="productDetails">
+          <p>&#x20B9;{postDetails.price} </p>
+          <span>{postDetails.name}</span>
+          <p>{postDetails.category}</p>
+          <span>{postDetails.createdAt}</span>
+        </div>
+      { userDetails && <div className="contactDetails">
+          <p>Seller details</p>
+          <p>{userDetails.username}</p>
+          <p>{userDetails.phone}</p>
+        </div> }
+      </div>
+    </div>
+  );
+}
+export default View;
---
+++ src/firebase/config.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+import 'firebase/auth'
+import 'firebase/firestore'
+import 'firebase/storage'
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+  };
+
+export default firebase.initializeApp(firebaseConfig)
